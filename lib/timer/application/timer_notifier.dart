971aea17c0b8539c5d37539cf605c972b9e098cf
Kalle Björklid <kalle.bjorklid@gmail.com>
2025-01-12 12:11:32 +0200

    refactor: Add timer functionality and cleanup in TimerNotifier

--- lib/timer/application/timer_notifier.dart
+++ lib/timer/application/timer_notifier.dart
@@ -1,7 +1,9 @@
+import 'dart:async';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pomodoro_app2/timer/domain/timer_state.dart';
 
 class TimerNotifier extends StateNotifier<TimerState> {
+  Timer? _timer;
   TimerNotifier()
       : super(TimerState(
           timerType: TimerType.work,
@@ -9,17 +11,38 @@ class TimerNotifier extends StateNotifier<TimerState> {
           isRunning: false,
         ));
 
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
   void toggleTimer() {
-    state = state.copyWith(isRunning: !state.isRunning);
+    if (state.isRunning) {
+      _timer?.cancel();
+      state = state.copyWith(isRunning: false);
+    } else {
+      state = state.copyWith(isRunning: true);
+      _timer = Timer.periodic(const Duration(seconds: 1), (_) {
+        if (state.remainingSeconds > 0) {
+          state = state.copyWith(remainingSeconds: state.remainingSeconds - 1);
+        } else {
+          _timer?.cancel();
+          state = state.copyWith(isRunning: false);
+        }
+      });
+    }
   }
 
   void switchTimerType() {
+    _timer?.cancel();
     final newType = state.timerType == TimerType.work
         ? TimerType.rest
         : TimerType.work;
     state = state.copyWith(
       timerType: newType,
       remainingSeconds: newType == TimerType.work ? 25 * 60 : 5 * 60,
+      isRunning: false,
     );
   }
 }
